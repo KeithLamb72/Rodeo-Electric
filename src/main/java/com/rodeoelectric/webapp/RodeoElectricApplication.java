4df5705ecaa83cbda3b63478766c93787d9785f9
Keith Lamb <keithianlamb@gmail.com>
2024-12-18 10:49:20 -0600

    The case study project was started through the start.spring.io initializer for dependencies and used the maven libraries.

---
+++ src/main/java/com/rodeoelectric/webapp/RodeoElectricApplication.java
@@ -0,0 +1,13 @@
+package com.rodeoelectric.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodeoElectricApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RodeoElectricApplication.class, args);
+	}
+
+}
